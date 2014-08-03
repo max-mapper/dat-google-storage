d76d3090b8f450b53b836c60c25182cb2109eea8
Max Ogden <max@maxogden.com>
2014-08-03 14:37:21 -0700

    use newer dat api

--- run.js
+++ run.js
@@ -17,33 +17,36 @@ var options = {
 var dgcs = require('./')
 
 var dat = new Dat('./data', function ready(err) {
-  dat.init(function(err) {
-    if (err) console.error(err)
-    dat.serve({port: port}, function() {
-      dgcs(options, function(err, clientRequest) {
-        importBucket()
-        
-        function importBucket(next) {
-          console.log('importing', next ? next : '')
-          var url = 'https://www.googleapis.com/storage/v1beta2/b/' + options.bucket + '/o'
-          if (next) url += '?pageToken=' + next
-          clientRequest({url: url}, function(err, resp, body) {
-            if (err) return console.error(err)
-            var ws = dat.createWriteStream({objects: true, primary: 'id'})
-            body.items.map(function(i) {
-              ws.write(i)
+  if (err) throw err
+  dat.listen(function(err, port) {
+    if (err) throw err
+    console.log(port)
+    dgcs(options, function(err, clientRequest) {
+      if (err) throw err
+      importBucket()
+    
+      function importBucket(next) {
+        console.log('importing', next ? next : '')
+        var url = 'https://www.googleapis.com/storage/v1beta2/b/' + options.bucket + '/o'
+        if (next) url += '?pageToken=' + next
+        clientRequest({url: url}, function(err, resp, body) {
+          if (err) return console.error(err)
+          var ws = dat.createWriteStream({objects: true, primary: 'id'})
+          body.items.map(function(i) {
+            i.key = i.id
+            dat.put(i, function(err){
+              if (err) console.error('put err', i, err)
             })
-            ws.end()
-            if (body.nextPageToken) {
-              importBucket(body.nextPageToken)
-            } else {
-              if (resp.statusCode < 299) console.log('finished import')
-              else console.log(resp.statusCode, body)
-              setTimeout(importBucket, 60000 * 60)
-            }
           })
-        }
-      })
+          if (body.nextPageToken) {
+            importBucket(body.nextPageToken)
+          } else {
+            if (resp.statusCode < 299) console.log('finished import')
+            else console.log(resp.statusCode, body)
+          }
+        })
+      }
     })
   })
+
 })
